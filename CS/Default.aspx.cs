880deef60db7dc644678a4fda8879b1eff3272a9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,8 @@
+using System;
+
+public partial class Grid_ClientSideEvents_ShowPopupOnContextMenu_ShowPopupOnContextMenu : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+}
