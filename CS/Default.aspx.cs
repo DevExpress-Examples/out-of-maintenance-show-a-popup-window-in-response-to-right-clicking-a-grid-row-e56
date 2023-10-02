57a1894abdeb92846b6b090bb2efe69010e65709
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 16:02:33 +0400

    Change example status to out of maintenance

--- CS/Default.aspx.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public partial class Grid_ClientSideEvents_ShowPopupOnContextMenu_ShowPopupOnContextMenu : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-    }
-}
--- VB/Default.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System
-
-Partial Public Class Grid_ClientSideEvents_ShowPopupOnContextMenu_ShowPopupOnContextMenu
-	Inherits System.Web.UI.Page
-
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-	End Sub
-End Class
